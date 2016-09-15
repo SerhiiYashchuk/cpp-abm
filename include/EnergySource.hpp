3d632b3193befe9b7a8a75836ec4b2b943f13561
Serhii Yashchuk <sergey.yaschuk@gmail.com>
2016-09-15 22:21:39 +0300

    Added basic EnergySource implementation

---
+++ include/EnergySource.hpp
@@ -0,0 +1,47 @@
+#ifndef ABM_ENERGY_SOURCE_HPP
+#define ABM_ENERGY_SOURCE_HPP
+
+#include <SFML/Graphics.hpp>
+
+namespace ABM
+{
+class EnergySource
+{
+public:
+  EnergySource() : EnergySource(0, 0, 0) {}
+  EnergySource(std::size_t maxCapacity, std::size_t currentLevel,
+               std::size_t regenerationRate,
+               sf::Vector2f position = sf::Vector2f(0, 0));
+
+  std::size_t getMaxCapacity() const { return maxCapacity; }
+  void setMaxCapacity(std::size_t value);
+
+  std::size_t getCurrentLevel() const { return currentLevel; }
+  void setCurrentLevel(std::size_t value);
+
+  std::size_t getRegenerationRate() const { return regenerationRate; }
+  void setRegenerationRate(std::size_t value) { regenerationRate = value; }
+
+  sf::Vector2f getPosition() const { return shape.getPosition(); }
+  void setPosition(sf::Vector2f value) { shape.setPosition(value); }
+
+  const sf::CircleShape & getShape() const { return shape; }
+
+  void regenerate();
+
+protected:
+
+private:
+  void updateShapeRadius();
+
+  std::size_t maxCapacity = 0;
+  std::size_t currentLevel = 0;
+  std::size_t regenerationRate = 0;
+  sf::CircleShape shape;
+
+  static constexpr auto minimumRadius = 5.f;
+  static constexpr auto maximumRadius = 30.f;
+};
+}
+
+#endif
---
+++ src/EnergySource.cpp
@@ -0,0 +1,80 @@
+#include "EnergySource.hpp"
+
+namespace ABM
+{
+
+/**
+ * @brief C-tor
+ * @param maxCapacity
+ * @param currentLevel
+ * @param regenerationRate
+ * @param position
+ */
+EnergySource::EnergySource(std::size_t maxCapacity, std::size_t currentLevel,
+                           std::size_t regenerationRate, sf::Vector2f position)
+  : maxCapacity(maxCapacity),
+    currentLevel(currentLevel),
+    regenerationRate(regenerationRate)
+{
+  shape.setPosition(position);
+  shape.setFillColor(sf::Color::Yellow);
+
+  updateShapeRadius();
+}
+
+/**
+ * @brief Sets maximum level of energy. If current level of energy is higher
+ * than the given value, it resets it to the specified maximum level
+ * @param value
+ */
+void EnergySource::setMaxCapacity(std::size_t value)
+{
+  if (currentLevel > value)
+  {
+    currentLevel = value;
+  }
+
+  maxCapacity = value;
+}
+
+/**
+ * @brief Sets current level of energy
+ * @param value
+ */
+void EnergySource::setCurrentLevel(std::size_t value)
+{
+  if (value > maxCapacity)
+  {
+    return;
+  }
+
+  updateShapeRadius();
+}
+
+/**
+ * @brief Accumulates energy
+ */
+void EnergySource::regenerate()
+{
+  if (regenerationRate == 0)
+  {
+    return;
+  }
+
+  currentLevel = std::min(currentLevel + regenerationRate, maxCapacity);
+
+  updateShapeRadius();
+}
+
+/**
+ * @brief Changes shape's radius in proportion to the current level of energy
+ */
+void EnergySource::updateShapeRadius()
+{
+  static constexpr auto radiusDifference = maximumRadius - minimumRadius;
+  const auto k = static_cast<float> (currentLevel) / maxCapacity;
+
+  shape.setRadius(minimumRadius + radiusDifference * k);
+}
+
+}
